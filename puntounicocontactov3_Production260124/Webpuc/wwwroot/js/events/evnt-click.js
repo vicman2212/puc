35e7344fd43c5e0b45bb9298e403fdbd638ab51a
Vicman <vrodriguez2212@gmail.com>
2024-01-28 07:53:59 -0600

    actualizacion enero

---
+++ puntounicocontactov3_Production260124/Webpuc/wwwroot/js/events/evnt-click.js
@@ -0,0 +1,271 @@
+﻿import { ajax } from "../soket.js";
+import { urlApp } from "../config.js"
+import { deployDirectorio, AccesoPUC } from "../misc/PeticionesHome.js"
+export function submit_eventos(ev) {
+
+    if (ev.target.matches('#btn-login')) {
+        ev.preventDefault()
+        AccesoPUC()
+    }
+    //Campo para reseteo de password
+    else if (ev.target.matches('#btn-reset-passwd')) {
+        ev.preventDefault()
+        let pass1 = document.getElementById('id-set-pass')
+        let pass2 = document.getElementById('id-reset-pass')
+        let usuario = document.getElementById('id-reset-usr')
+        let alertaMsn = document.getElementById('alertb-reset-pass')
+        //Establecer que los campos no pueden estar vacios
+        if (pass1.value != "" && pass2.value != "") {
+            //Comparar que el campo pass 1 y pass 2 sean iguales
+            if (pass1.value === pass2.value) {
+                // hacer peticion al servidor
+                let datos = new FormData();
+                datos.append("Password", pass2.value)
+                datos.append("Usuario", usuario.value)
+
+                ajax({
+                    url: urlApp + "Home/ResetPasswd",
+                    method: "POST",
+                    cbSucced(respuesta) {
+                        if (respuesta == "1") {//Redireccionar a login principal
+                            location.href = urlApp + "Home/Login"
+                        } else if (respuesta == "00") {
+                            pass1.value = ""
+                            pass2.value = ""
+                            swal("¡Atención!", "No se permite ingresar la misma contraseña.", "warning"); 
+                            //alertaMsn.innerHTML = "¡Atención! USTED ESTA INGRESANDO LA MISMA CONTRASEÑA. ELIJA OTRA."
+                            //$("#modalId-resetPwd").modal("show")
+                        }
+                        else {
+                            swal("¡Atención!", "Ocurrio un error inesperado", "Error");                         
+                        }
+
+                    }
+                }, datos);
+            } else {// Los campos no coiciden error de usuario
+                pass2.value = ""
+                swal("¡Atención!", "Las contraseñas ingresadas no coinciden", "warning"); 
+                //alertaMsn.innerHTML = "¡Atención! Las contraseñas ingresadas no coinciden"
+                //$("#modalId-resetPwd").modal("show")
+            }
+        } else {//Los campos van vacios error de usuario
+            swal("¡Atención!", "Las contraseñas ingresadas no coinciden", "warning");
+            //alertaMsn.innerHTML = "¡Atención! Ninguno de los campos deben de estar vacios"
+            //$("#modalId-resetPwd").modal("show")
+        }
+       
+
+    }
+    //Para el proceso del directorio
+    else if (ev.target.matches('#btn-busqueda-rgx')) {// BUSQUEDA POR EXPRESION REGULAR
+        ev.preventDefault()
+        let regx = document.getElementById('Dir-bus-rgx').value
+        regx = regx.replace("Á", "A");
+        regx = regx.replace("É", "E");
+        regx = regx.replace("Í", "I");
+        regx = regx.replace("Ó", "O");
+        regx = regx.replace("Ú", "U");
+        regx = regx.replace("Ü", "U");
+        let datos = new FormData()
+        datos.append('rgex', regx)
+        if (regx.length < 2) {
+            swal("¡Atención!", "solo se acepta un mínimo de 2 caracteres", "warning");
+            return false
+        }
+        document.getElementById('Dir-bus-rgx').value = "" //Resetar busqueda
+        let urlcontroller = urlApp + "Directorio/LocateByRegex"
+        deployDirectorio(datos, urlcontroller)
+    } else if (ev.target.matches('#btn-sc-admemp')) {//Cuando se va a guardar un cambio de usuario
+        ev.preventDefault()
+        let re = /[0-9]/ig
+        let html_empleado = document.getElementById("id-no-emp").innerHTML
+        let selected_ubicacion = document.getElementById("change-entidad-id").value
+        let selected_area = document.getElementById("change-area-id").value
+        let selected_puesto = document.getElementById("change-puesto-id").value
+        let selected_status = document.getElementById("change-status").value
+        let input_extension = document.getElementById("change-ext").value
+        let old_dbExt = document.getElementById("db-ext").innerHTML
+        let notific_modal = document.getElementById("notific-modal-adminEmp")
+        //Establecer los datos a usar
+        let datos = new FormData()
+        datos.append("NoEmpleado", html_empleado)
+        datos.append("IdDirDeparment", selected_area)
+        datos.append("IdJob", selected_puesto)
+        datos.append("IsActive", selected_status)
+        datos.append("Ext", input_extension)
+
+        //alert(selected_ubicacion + selected_area + selected_puesto + "ext" + input_extension)
+        if (re.test(input_extension)) {
+            //alert("coincide el formato")
+            if (selected_ubicacion != 0) {// Se esta modificando la ubicacion
+                //Validar que area y puesto no se vayan en 0, es obligatorio asignar parametros
+                if (selected_area == 0 && selected_puesto == 0) {
+                    //Enviar alerta para que ingresen los datos
+                    swal("¡Atención! Se esta modificando la ubicacion", "Hay que reasignar el area y el pueto", "warning");
+                    //document.getElementById('msn-alert-adminEmp').innerHTML = `<div class="alert alert-warning">
+                    //                    <strong>¡Atencion!</strong> Recuerda que se esta modificando la ubicacion del empleado, por lo que tendrá que reasignar el <strong>ÁREA</strong> y <strong>PUESTO</strong>
+                    //                    </div>`
+                } else {//Se llenaron todos los campos de manera correcta
+                    // Realizar la peticion Ajax
+                    ajax({
+                        url: "/AdminEmpleado/UpdateEmpleado",
+                        method: "POST",
+                        cbSucced: function (respuesta) {
+                            //var json = JSON.parse(respuesta)
+                            swal("¡EXITO!", "Se actualizaro los datos del empleado", "success")
+                            //notific_modal.innerHTML = "¡EXITO! Usted actualizo los datos del empleado"
+                            //$("#Modal-adminEmp").modal("show");
+
+                        }
+                    }, datos)
+                }
+            }
+            else if (selected_area != 0) { //Solo se esta actualizando el Area
+                if (selected_puesto == 0) {//Validar que se esta reasignando el puesto
+                    //Enviar alerta para que ingresen los datos
+                    swal("¡ATENCIÓN!", "Solo se esta modificando el área, hay que reasignar el PUESTO", "warning")
+                    //document.getElementById('msn-alert-adminEmp').innerHTML = `<div class="alert alert-warning">
+                    //                    <strong>¡Atencion!</strong> Solo esta modificando el Área, por lo que tendrá que reasignar el <strong>PUESTO</strong>
+                    //                    </div>`
+                } else {
+                    ajax({
+                        url: "/AdminEmpleado/UpdateEmpleado",
+                        method: "POST",
+                        cbSucced: function (respuesta) {
+                            //var json = JSON.parse(respuesta)
+                            swal("¡EXITO!", "Se actualizo el Área y PUESTO del empleado", "success")
+                            //notific_modal.innerHTML = "¡EXITO! Usted actualizo el ÁREA del empleado y el PUESTO"
+                            //$("#Modal-adminEmp").modal("show");
+                        }
+
+                    }, datos)
+                }
+            }
+            else if (selected_puesto != 0) {//Solo se modifica el puesto
+                //alert("Solo esta modificando el puesto")
+                ajax({
+                    url: "/AdminEmpleado/UpdateEmpleado",
+                    method: "POST",
+                    cbSucced: function (respuesta) {
+                        //var json = JSON.parse(respuesta)
+                        swal("¡EXITO!", "Se ha actualizado el puesto", "success")
+                        //notific_modal.innerHTML = "¡EXITO! Usted actualizo el PUESTO"
+                        //$("#Modal-adminEmp").modal("show");
+                    }
+
+                }, datos)
+            }
+            else if (selected_status != 1) {//Solo se modifica el status
+                ajax({
+                    url: "/AdminEmpleado/UpdateEmpleado",
+                    method: "POST",
+                    cbSucced: function (respuesta) {
+                        //var json = JSON.parse(respuesta)
+                        swal("¡EXITO!", "usted DESHABILITO AL EMPLEADO", "success")
+                        //notific_modal.innerHTML = "¡EXITO! Usted DESHABILITO AL EMPLEADO"
+                        //$("#Modal-adminEmp").modal("show");
+                    }
+
+                }, datos)
+            }
+            else if (input_extension != old_dbExt) {//Cuando sea diferente la extension
+                ajax({
+                    url: urlApp + "AdminEmpleado/UpdateEmpleado",
+                    method: "POST",
+                    cbSucced: function (respuesta) {
+                        //var json = JSON.parse(respuesta)
+                        swal("¡EXITO!", "Se actualizo la extensión ", "success")
+                        notific_modal.innerHTML = "¡EXITO! Actualizo la extensión"
+                        $("#Modal-adminEmp").modal("show");
+                    }
+                }, datos)
+            }
+            else {
+                swal("¡Atención!", "No se estan enviando datos cancele la operacion", "warning")
+                //document.getElementById('msn-alert-adminEmp').innerHTML = `<div class="alert alert-warning">
+                //                        <strong>¡Atencion!</strong> No esta enviando datos, CANCELE la operación</strong>
+                //                        </div>`
+            }
+        } else {
+            swal("¡Atención!", "No coincide el formato de la extención", "warning")
+            //alert("no coincide el formato de la extension")
+        }
+        //Validaciones: No se modificar la ubicacion        
+        /**fin proc if/else*/
+    }else if (ev.target.matches('#submit-insertEmp')) {
+        ev.preventDefault()
+        //seleccionar campos del usuario
+        let inputNoEmpleado = document.getElementById('id-NoEmpleado')
+        let inputFName = document.getElementById('id-FirstName')
+        let inputSecondName = document.getElementById('id-SecondName')
+        let inputApeP = document.getElementById('id-ApeP')
+        let inputApeM = document.getElementById('id-ApeM')
+        let inputExt = document.getElementById('id-ext')
+        //Validar que no vayan vacios los elementos para insertar un nuevo empleado
+        let select_ef = document.getElementById('change-entidad-id').value
+        let select_dep = document.getElementById('change-area-id').value
+        let select_puesto = document.getElementById('change-puesto-id').value
+
+        if (select_ef === '0' || select_dep === '0' || select_puesto === '0') {
+            swal("¡Atención!", "Elija la ubicacion y puesto del usuario", "warning")
+            //MsnAlertInsertEmp("!Atencion! Elija la ubicación y puesto del usuario")
+        } else if (inputNoEmpleado.value === "" || inputFName.value === "" ||
+            inputApeP.value === "" || inputApeM.value === "") {
+            swal("¡Atención!", "ningun campo debe de estar vacio", "warning")
+            //MsnAlertInsertEmp("Atención, ningun campo debe estar vacio.")
+        } else {
+            let nombres = inputFName.value + " " + inputSecondName.value
+            let datos = new FormData()
+            datos.append("NoEmpleado", inputNoEmpleado.value)
+            datos.append("Nombres", nombres)
+            datos.append("ApellidoPaterno", inputApeP.value)
+            datos.append("ApellidoMaterno", inputApeM.value)
+            datos.append("Extension", inputExt.value)
+            datos.append("DepartamentoId", select_dep)
+            datos.append("PuestoId", select_puesto)
+            //monica2023_2023
+            //alert("Peticion AJAX")
+            ajax({
+                url: urlApp + "AdminEmpleado/InsertEmpleado",
+                method: "POST",
+                cbSucced: function (respuesta) {
+                    if (respuesta === "true") {
+                        //Limpiar campos 
+                        inputNoEmpleado.value = ""
+                        inputFName.value = ""
+                        inputSecondName.value = ""
+                        inputApeP.value = ""
+                        inputApeM.value = ""
+                        inputExt.value = ""
+                        inputNoEmpleado.removeAttribute("style")
+                        inputFName.removeAttribute("style")
+                        inputSecondName.removeAttribute("style")
+                        inputApeP.removeAttribute("style")
+                        inputApeM.removeAttribute("style")
+                        //Evento para resetear la lista desplegable seleccionada 
+                        $('#change-entidad-id option').prop('selected', function () {
+                            return this.defaultSelected;
+                        });
+                        $('#change-area-id option').prop('selected', function () {
+                            return this.defaultSelected;
+                        });
+                        $('#change-puesto-id option').prop('selected', function () {
+                            return this.defaultSelected;
+                        });
+                        swal("¡Exito!", "Se ha notificado del ingreso de un nuevo usuario", "success")
+                        //MsnAlertInsertEmp(`Se agrego al empleado con exito, en breve recibira la notificación`)
+                    }
+                }
+            }, datos)
+        }
+    }
+
+}
+
+//function MsnAlertInsertEmp(msn) {
+//    const alertMsn = document.getElementById('alert-insert-emp')
+//    alertMsn.setAttribute("class","modal-body alert alert-succeded")
+//    alertMsn.innerHTML = ""
+//    alertMsn.innerHTML = msn
+//    $('#modalId-insertEmp').modal("show");
+//}
---
+++ puntounicocontactov3_Production260124/Webpuc/wwwroot/js/events/evnt-focusout.js
@@ -0,0 +1,141 @@
+﻿//import { ajax } from "../soket.js";
+import { usr_cred, empleado_form } from "../validations/client-validation.js"
+import { insertAlert } from "../alertas/cint-alerts.js"
+import { ajax } from "../soket.js"
+import { urlApp } from "../config.js"
+export function focusout_eventos(ev) {
+
+    if (ev.target.matches('#id-usuario')) {// BUSQUEDA POR EXPRESION REGULAR
+        ev.preventDefault()
+        let $inputUsr = document.getElementById('id-usuario')
+        let msn = document.getElementById('msn-login')
+        //Validar que no se encuentre vacion el elemento de usuario
+        if ($inputUsr.value != "") {
+            if (!usr_cred.cbUsuario($inputUsr.value)) {//No coincide con la expresion regular del campo
+                msn.innerHTML = ""
+                swal("¡Atención!", "El usuario no es valido", "error");
+                //const alert = new insertAlert(msn, "¡ATENCIÓN!", "el usuario no es valido")
+                //alert.msnAlertWarning('msn-login');
+                $inputUsr.value = ""
+            } else {
+                msn.innerHTML = ""
+                msn.removeAttribute("class")
+            }
+        }
+
+    }else if (ev.target.matches('#id-set-pass')) {
+        ev.preventDefault()
+        let $inputPass = document.getElementById('id-set-pass')
+        let msn = document.getElementById('msn-reset-pass')
+        if (!usr_cred.cbPass($inputPass.value)) {//No coincide con la expresion regular del campo
+            swal("¡Atención!", "La contraseña NO cumple con las reglas de seguridad", "warning");
+            //const alert = new insertAlert(msn, "¡ATENCIÓN!", "la contraseña ingresada NO cumple las reglas de seguridad")
+            //alert.msnAlertDanger('msn-reset-pass');
+            $inputPass.value = ""
+        } else {
+            msn.innerHTML = ""
+            msn.removeAttribute("class")
+        }
+    }
+    //************************************************************************************* */
+    //Validaciones para ingresar un usuario 
+    else if (ev.target.matches('#id-NoEmpleado')) {
+        ev.preventDefault()
+        let inputNoEmpleado = document.getElementById('id-NoEmpleado')
+        let nameField = inputNoEmpleado.getAttribute('placeholder')
+
+        if (inputNoEmpleado.value != "") {
+            if (!empleado_form.cbNoEmpleado(inputNoEmpleado.value)) {
+                swal("¡Atención!", `No cumple la regla para el campo ${nameField}`, "warning")
+                //MsnAlertInsertEmp(`No cumple la regla establecida para el campo ${nameField}`)
+                inputNoEmpleado.value = ""
+            } else {
+                //Consultamos en la base de datos que el id ingresado no exista.
+                let datos = new FormData()
+                datos.append("NoEmpleado", inputNoEmpleado.value)
+
+                ajax({
+                    url: urlApp + "AdminEmpleado/ExistNoEmpleado",
+                    method: "POST",
+                    cbSucced: function (respuesta) {
+                        //var json = JSON.parse(respuesta)
+                        if (respuesta == "true") {//Si no se encontro el id          
+                            inputNoEmpleado.setAttribute("style", "background-color:white")
+                            inputNoEmpleado.value = ""
+                            swal("¡Atención!", "Ya existe este empleado", "warning")
+                            //MsnAlertInsertEmp("Atencion este número de empleado ya existe")
+                        } else {
+                            inputNoEmpleado.setAttribute("style", "background-color:#d1e7dd")
+                        }
+                    }
+                }, datos)
+            }
+        }
+
+    } else if (ev.target.matches('#id-FirstName')) {
+        ev.preventDefault()
+        let inputFName = document.getElementById('id-FirstName')
+        let nameField = inputFName.getAttribute('placeholder')
+
+        if (inputFName.value != "") {
+            if (!empleado_form.cbFirstName(inputFName.value)) {
+                swal("¡Atención!", `No cumple la regla para el campo ${nameField}`, "warning")
+                //MsnAlertInsertEmp(`No cumple la regla para el campo ${nameField}`)
+                inputFName.value = ""
+            } else {
+                inputFName.setAttribute("style", "background-color:#d1e7dd")
+            }
+        }
+       
+    } else if (ev.target.matches('#id-SecondName')) {
+        ev.preventDefault()
+        let inputFName = document.getElementById('id-SecondName')
+        let nameField = inputFName.getAttribute('placeholder')
+        if (inputFName.value != "") {
+            if (!empleado_form.cbFirstName(inputFName.value)) {
+                swal("¡Atención!", `No cumple la regla para el campo ${nameField}`, "warning")
+                //MsnAlertInsertEmp(`No cumple la regla para el campo ${nameField}`)
+                inputFName.value = ""
+            } else {
+                inputFName.setAttribute("style", "background-color:#d1e7dd")
+            }
+        }
+
+    } else if (ev.target.matches('#id-ApeP')) {
+        ev.preventDefault()
+        let inputApeP = document.getElementById('id-ApeP')
+        let nameField = inputApeP.getAttribute('placeholder')
+        //let inputApeP = document.getElementsByClassName('class-Apell')
+        if (inputApeP.value != "") {
+            if (!empleado_form.cbapellidos(inputApeP.value)) {
+                swal("¡Atención!", `No cumple la regla para el campo ${nameField}`, "warning")
+                //MsnAlertInsertEmp(`No cumple la regla para el campo`)
+                inputApeP.value = ""
+            } else {
+                inputApeP.setAttribute("style", "background-color:#d1e7dd")
+            }
+        }
+    } else if (ev.target.matches('#id-ApeM')) {
+        ev.preventDefault()
+        let inputApeM = document.getElementById('id-ApeM')
+        let nameField = inputApeM.getAttribute('placeholder')
+        if (inputApeM.value != "") {
+            if (!empleado_form.cbapellidos(inputApeM.value)) {
+                swal("¡Atención!", `No cumple la regla para el campo ${nameField}`, "warning")
+                //MsnAlertInsertEmp(`No cumple la regla para el campo`)
+                inputApeM.value = ""
+            } else {
+                inputApeM.setAttribute("style", "background-color:#d1e7dd")
+            }
+        }
+    }
+
+}
+
+//Funciones externas
+function MsnAlertInsertEmp(msn) {
+    const alertMsn = document.getElementById('alert-insert-emp')
+    alertMsn.innerHTML = ""
+    alertMsn.innerHTML = msn
+    $('#modalId-insertEmp').modal("show");
+}
